385b23bf877e231c87a86e14eda0fc09947ff806
Johan <johasel@stud.ntnu.no>
2019-11-13 11:11:15 +0100

    error check

--- pkg/riot/riot.go
+++ pkg/riot/riot.go
@@ -27,6 +27,9 @@ func New(client models.Client, apiKey string) *Riot {
 func (r *Riot) GetRiotPlaytime(reg models.SummonerRegistration) (*models.Game, error) {
 	logrus.Debugf("GetLolPlaytime")
 	logrus.Debugf("reg: %+v", reg)
+	if reg.SummonerRegion == "" || reg.AccountID == "" {
+		return nil, fmt.Errorf("missing summonerinfo")
+	}
 
 	URL := fmt.Sprintf("https://%s.api.riotgames.com/lol/match/v4/matchlists/by-account/%s?beginIndex=99999", reg.SummonerRegion, reg.AccountID)
 
