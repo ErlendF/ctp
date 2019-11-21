dadc41f0b429503ab41f419593f08a0a8b458336
Erlend Fonnes <erlend.fonnes@gmail.com>
2019-11-21 11:12:55 +0100

    Added test of authcallback in user

--- pkg/user/manager_test.go
+++ pkg/user/manager_test.go
@@ -4,10 +4,13 @@ import (
 	"ctp/pkg/models"
 	"errors"
 	"net/http"
+	"net/http/httptest"
+	"strings"
 	"testing"
 
 	"github.com/bxcodec/faker"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
 type mockDB struct {
@@ -58,11 +61,13 @@ func TestSetUser(t *testing.T) {
 		name        string
 		orgErr      error
 		dbErr       error
+		dbUserEqual bool
 		expectedErr error
 	}{
-		{"Test ok", nil, nil, nil},
-		{"Test orgErr", errors.New("test"), nil, errors.New("test")},
-		{"Test orgErr", nil, errors.New("test"), errors.New("test")},
+		{"Test ok", nil, nil, false, nil},
+		{"Test orgErr", errors.New("test"), nil, false, errors.New("test")},
+		{"Test dbErr", nil, errors.New("test"), false, errors.New("test")},
+		{"Test dbuser equal", nil, nil, true, nil},
 	}
 
 	db := &mockDB{}
@@ -77,13 +82,52 @@ func TestSetUser(t *testing.T) {
 			assert.NoError(t, err)
 			user.Name = "testuser123"
 
-			err = faker.FakeData(&db.user)
-			assert.NoError(t, err)
+			if tc.dbUserEqual {
+				db.user = user
+			} else {
+				err = faker.FakeData(&db.user)
+				assert.NoError(t, err)
+			}
 			db.err = tc.dbErr
 			fakeOrg(t, org, tc.orgErr)
 
 			err = um.SetUser(user)
 			assert.Equal(t, tc.expectedErr, err)
+		})
+	}
+}
+
+func TestAuthCallback(t *testing.T) {
+	var cases = []struct {
+		name        string
+		orgErr      error
+		dbErr       error
+		expectedErr error
+	}{
+		{"Test ok", nil, nil, nil},
+		{"Test orgErr", errors.New("test"), nil, errors.New("test")},
+		{"Test dbErr", nil, errors.New("test"), errors.New("test")},
+	}
+
+	db := &mockDB{}
+	org := &mockOrganizer{}
+	um := New(db, org)
+
+	// tc - test cases
+	for _, tc := range cases {
+		t.Run(tc.name, func(t *testing.T) {
+			db.err = tc.dbErr
+			fakeOrg(t, org, tc.orgErr)
+
+			// Making and serving request
+			r, err := http.NewRequest(http.MethodGet, "test", strings.NewReader("test"))
+			require.Nil(t, err)
+
+			w := httptest.NewRecorder()
+			token, err := um.AuthCallback(w, r)
+			if assert.Equal(t, tc.expectedErr, err) && err == nil {
+				assert.Equal(t, org.token, token)
+			}
 		})
 	}
 }
