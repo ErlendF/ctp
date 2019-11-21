b95ca85fbb9140e87b1c06e28558453fc0a78ba2
Aksel Baardsen <akselba@stud.ntnu.no>
2019-11-21 07:41:26 +0100

    Added custom Do() to riot test

--- pkg/riot/riot_test.go
+++ pkg/riot/riot_test.go
@@ -1,14 +1,41 @@
 package riot
 
-import "net/http"
+import (
+	"bytes"
+	"encoding/json"
+	"io/ioutil"
+	"net/http"
+	"strings"
+	"testing"
+)
 
+// mockClient is used for setting up the test
 type mockClient struct {
+	setup string
 	err error
 }
 
-func (m *mockClient) Do(req *http.Request) (resp *http.Response, err error) {
+// Do mocks a httpRequest.Do() for testing
+func (m *mockClient) Do(req *http.Request) (*http.Response, error) {
+	// if a wrong url is sent, return default error
+	if !strings.Contains(req.URL.Path, "api.riotgames.com/lol/match/v4") {
+		return nil, m.err
+	}
 
+	// set http response header
+	resp := &http.Response{StatusCode: http.StatusOK, Header: make(http.Header)}
 
+	// add preconfigured response body to THIS response
+	body, err := json.Marshal(m.setup)
+	if err != nil {
+		return nil, err
+	}
+	resp.Body = ioutil.NopCloser(bytes.NewReader(body))
 
-	return nil, nil
+	// successful request
+	return resp, nil
+}
+
+func TestRiot_ValidateSummoner(t *testing.T) {
+
 }
