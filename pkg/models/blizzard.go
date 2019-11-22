19d58cdd7b223b93a65bd58c96667643e678e21f
Erlend Fonnes <erlend.fonnes@gmail.com>
2019-11-22 21:18:14 +0100

    Removed unnecessary fields from structs, and moved structs to more relevant locations

--- pkg/models/blizzard.go
+++ pkg/models/blizzard.go
@@ -6,25 +6,6 @@ type Blizzard interface {
 	ValidateBattleUser(overwatch *Overwatch) error
 }
 
-// BlizzardResp struct retrieves only time played in Overwatch
-type BlizzardResp struct {
-	CompetitiveStats struct {
-		CareerStats CareerStats `json:"careerStats"`
-	} `json:"competitiveStats"`
-	QuickPlayStats struct {
-		CareerStats CareerStats `json:"careerStats"`
-	} `json:"quickPlayStats"`
-}
-
-// CareerStats includes time played for all heroes by game
-type CareerStats struct {
-	AllHeroes struct {
-		Game struct {
-			TimePlayed string `json:"timePlayed"`
-		} `json:"game"`
-	} `json:"allHeroes"`
-}
-
 // Overwatch struct contains users battle tag and total playtime
 type Overwatch struct {
 	BattleTag string `json:"battleTag" firebase:"battleTag"`
